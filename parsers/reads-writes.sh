0fe385ca2433e2de699ca8eb13378dcc5423d537
payal <payal@payal-ThinkPad-T520.(none)>
2014-02-17 17:51:07 -0500

    Adding generic read-write parser

---
+++ parsers/reads-writes.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# $1 disk name
+# $2 column name
+# $3 type
+
+set -x
+set -e
+
+col=$(echo $2 | sed "s/\//p/g")
+echo "date"'\t'"$col" > $3-$1.tsv
+pos=$(cat iostat-*.log | sed -e "s/\s\{1,\}/ /g" | grep $2 | awk -v var=$2 '{len=split($0, a, " "); for(i=1;i<=len;i++){if (a[i] == var) print i;}}' | head -1)
+cat iostat-*.log | grep $1 | awk -v position=$pos '{print $2"\t"$position}'>> $3-$1.tsv
