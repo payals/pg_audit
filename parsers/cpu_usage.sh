270d137cac4b228d490cb52976ede8ad19051771
payal <payal@payal-ThinkPad-T520.(none)>
2014-02-17 21:16:24 -0500

    Adding cpu_usage parser

--- parsers/cpu_usage.sh
+++ parsers/cpu_usage.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-# $1 disk name
+# $1 file name
 # $2 usr column name
 # $3 sys column name
 # $4 iowait column name
@@ -11,5 +11,5 @@ set -e
 usr=$(cat mpstat-*.log | sed -e "s/\s\{1,\}/ /g" | grep $2 | awk -v var=$2 '{len=split($0, a, " "); for(i=1;i<=len;i++){if (a[i] == var) print i;}}' | head -1)
 sys=$(cat mpstat-*.log | sed -e "s/\s\{1,\}/ /g" | grep $3 | awk -v var=$3 '{len=split($0, a, " "); for(i=1;i<=len;i++){if (a[i] == var) print i;}}' | head -1)
 iowait=$(cat mpstat-*.log | sed -e "s/\s\{1,\}/ /g" | grep $4 | awk -v var=$4 '{len=split($0, a, " "); for(i=1;i<=len;i++){if (a[i] == var) print i;}}' | head -1)
-cat mpstat-*.log | grep -v 'Linux\|usr\|CPU' | awk -v usr=$usr -v sys=$sys -v iowait=$iowait  '{print $2"\t"$usr"\t"$sys"\t"$iowait}' | awk 'BEGIN {FS="\t"} $2!="" {print}' > cpu_usage.tsv
-sed -i "1s/^/date\t$2\t$3\t$4\n&/" cpu_usage.tsv
+cat mpstat-*.log | grep -v 'Linux\|usr\|CPU' | awk -v usr=$usr -v sys=$sys -v iowait=$iowait  '{print $2"\t"$usr"\t"$sys"\t"$iowait}' | awk 'BEGIN {FS="\t"} $2!="" {print}' > $1_cpu_usage.tsv
+sed -i "1s/^/date\t$2\t$3\t$4\n&/" $1_cpu_usage.tsv
