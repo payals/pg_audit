f0211051b8d77535fcc0058b378301428dd09fb0
payal <payal@payal-ThinkPad-T520.(none)>
2014-02-17 20:24:56 -0500

    Adding $ before tabspace in echo, added waiting lock parser

--- parsers/locks.sh
+++ parsers/locks.sh
@@ -21,3 +21,13 @@ sed -i '1s/^/date\trowExclusiveLock\n&/' RowExclusiveLock.tsv
 sed -i '1s/^/date\texclusiveLock\n&/' ExclusiveLock.tsv
 sed -i '1s/^/date\trowExclusiveLock\texclusiveLock\n&/' AllExclusiveLocks.tsv
 sed -i '1s/^/date\taccessShareLock\tshareLock\n&/' AllShareLocks.tsv
+
+# Waiting
+
+cut -f1,15,16 pg_locks-*.log | grep -v mode | grep ShareLock | sort | uniq -c | awk '$6 == "f" {print $3"\t"$1}' > SLTemp.tsv
+cut -f1,15,16 pg_locks-*.log | grep -v mode | grep AccessShareLock | sort | uniq -c | awk '$6 == "f" {print $3"\t"$1}' > ASLTemp.tsv
+cut -f1,15,16 pg_locks-*.log | grep -v mode | grep ExclusiveLock | sort | uniq -c | awk '$6 == "f" {print $3"\t"$1}' > ELTemp.tsv
+cut -f1,15,16 pg_locks-*.log | grep -v mode | grep RowExclusiveLock | sort | uniq -c | awk '$6 == "f" {print $3"\t"$1}' > RELTemp.tsv
+
+join -a1 -a2 -1 1 -2 1 -o 0 1.2 2.2 -e "0" ASLTemp.tsv SLTemp.tsv | awk {'print $1"\t"$2"\t"$3'} > SharedWaiting.tsv
+join -a1 -a2 -1 1 -2 1 -o 0 1.2 2.2 -e "0" RELTemp.tsv ELTemp.tsv | awk {'print $1"\t"$2"\t"$3'} > ExclusiveWaiting.tsv
