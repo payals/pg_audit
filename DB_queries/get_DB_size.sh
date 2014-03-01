10cecd004b84cf6e9f75db6ba2d34ea857aa25e0
payal <payal@payal-ThinkPad-T520.(none)>
2014-03-01 01:21:22 -0500

    Adding new directories to rectify path issues

---
+++ DB_queries/get_DB_size.sh
@@ -0,0 +1,10 @@
+# Global configuration, log directory
+PSQL_PATH=/opt/pgsql/bin/psql
+PGUSER=payal
+PGDATABASE=flpg
+
+set -e
+set -x
+
+# Get database size
+$PSQL_PATH -c "COPY(select datname as database, pg_database_size(datname) as size from pg_database) TO '$HOME/pg_audit/input/db_size.csv' WITH (FORMAT csv , DELIMITER ',' , HEADER true)" -U $PGUSER -d $PGDATABASE
